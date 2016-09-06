2ed2e57c09dae57c4d57f73fba87e3ad729b9920
Coen Zimmerman <coen@pixelindustries.com>
2016-09-06 19:12:56 +0200

    Updated translations for models index

--- resources/lang/en/models.php
+++ resources/lang/en/models.php
@@ -2,9 +2,24 @@
 
 return [
 
+    'no-records-found' => 'no :name found.',
+
+    'button' => [
+        'new-record' => 'New :name',
+    ],
+
 	'filter' => [
 	    'title'        => 'Filter:',
 	    'button-label' => 'Filter',
+    ],
+
+    'scope' => [
+        'all' => 'All',
+    ],
+
+    'counts' => [
+        'out-of' => '/',
+        'total'  => 'total',
     ],
 
 ];
--- resources/lang/nl/models.php
+++ resources/lang/nl/models.php
@@ -2,9 +2,24 @@
 
 return [
 
+    'no-records-found' => 'Geen :name gevonden.',
+
+    'button' => [
+        'new-record' => 'Voeg :name toe',
+    ],
+
 	'filter' => [
 	    'title'        => 'Filter:',
 	    'button-label' => 'Filter',
+    ],
+
+    'scope' => [
+        'all' => 'Alles',
+    ],
+
+    'counts' => [
+        'out-of' => '/',
+        'total'  => 'totaal',
     ],
 
 ];
