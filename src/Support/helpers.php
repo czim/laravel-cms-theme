8bdb3b5185123ab6611954f7641b1df1bdcad6e5
Coen Zimmerman <coen@pixelindustries.com>
2016-09-26 20:03:30 +0200

    cms_trans function helpers now fall back to application translations

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -5,6 +5,8 @@ if ( ! function_exists('cms_trans')) {
     /**
      * Translate the given message using CMS translations.
      *
+     * Falls back to application translations if CMS does not have the key.
+     *
      * @param  string  $id
      * @param  array   $parameters
      * @param  string  $domain
@@ -13,11 +15,21 @@ if ( ! function_exists('cms_trans')) {
      */
     function cms_trans($id = null, $parameters = [], $domain = 'messages', $locale = null)
     {
+        /** @var \Illuminate\Translation\Translator $translator */
+        $translator = app('translator');
+
         if (is_null($id)) {
-            return app('translator');
+            return $translator;
         }
 
-        return app('translator')->trans('cms::' . $id, $parameters, $domain, $locale);
+        $key = 'cms::' . $id;
+
+        // Fall back to non-cms translation if unavailable for CMS
+        if ( ! $translator->has($key)) {
+            $key = $id;
+        }
+
+        return $translator->trans($key, $parameters, $domain, $locale);
     }
 }
 
@@ -25,6 +37,8 @@ if ( ! function_exists('cms_trans_choice')) {
 
     /**
      * Translates the given message based on a count using CMS-translations.
+     *
+     * Falls back to application translations if CMS does not have the key.
      *
      * @param  string  $id
      * @param  int|array|\Countable  $number
@@ -35,6 +49,16 @@ if ( ! function_exists('cms_trans_choice')) {
      */
     function cms_trans_choice($id, $number, array $parameters = [], $domain = 'messages', $locale = null)
     {
-        return app('translator')->transChoice('cms::' . $id, $number, $parameters, $domain, $locale);
+        /** @var \Illuminate\Translation\Translator $translator */
+        $translator = app('translator');
+
+        $key = 'cms::' . $id;
+
+        // Fall back to non-cms translation if unavailable for CMS
+        if ( ! $translator->has($key)) {
+            $key = $id;
+        }
+
+        return app('translator')->transChoice($key, $number, $parameters, $domain, $locale);
     }
 }
