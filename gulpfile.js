0d7280dd5790cc9badc29662ceb75792aa9358fd
Coen Zimmerman <coen@pixelindustries.com>
2016-09-26 20:12:25 +0200

    Javascript update, added jquery sortable plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix
         .sass('app.scss', 'resources/assets/build/css')
-        .browserify('app.js', 'resources/assets/build/js/app.js');
+        .browserify('app.js', 'resources/assets/build/js/app.js')
+        .browserify('jquery-sortable.js', 'resources/assets/build/js/jquery-sortable.js');
 });
