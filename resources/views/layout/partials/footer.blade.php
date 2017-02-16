c88c1624aee3c57a9bbff8dfc08bd6cc9a8883e9
Coen Zimmerman <coen@pixelindustries.com>
2017-02-16 21:38:13 +0100

    Updated view template with references to local assets

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -3,23 +3,11 @@
 
 
 <!-- JavaScripts -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"
-        integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb"
-        crossorigin="anonymous"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
-        integrity="sha256-KM512VNnjElC30ehFwehXjx1YCHPiQkOPmqnrWtpccM="
-        crossorigin="anonymous"></script>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"
-        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-        crossorigin="anonymous"></script>
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/metisMenu/2.5.2/metisMenu.min.js"
-        integrity="sha256-FvtGTJgCbPmWr0DdIsMWeukxoOxWhWTCjT345wTj5Y8="
-        crossorigin="anonymous"></script>
-
-<script src="//cdn.ckeditor.com/4.5.11/full/ckeditor.js"></script>
-
+<script src="{{ asset('_cms/js/jquery.min.js') }}"></script>
+<script src="{{ asset('_cms/js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('_cms/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('_cms/js/metisMenu.min.js') }}"></script>
+<script src="{{ asset('_cms/js/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('_cms/js/app.js') }}"></script>
 <script src="{{ asset('_cms/js/jquery-sortable.js') }}"></script>
 <script src="{{ asset('_cms/js/jquery-select2.min.js') }}"></script>
--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -8,22 +8,17 @@
 
     <title>{{ config('cms-theme.header.title', 'CMS') }} - @yield('title', 'Dashboard')</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha256-rByPlHULObEjJ6XQxW/flG2r+22R5dKiAoef+aXWfik=" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/metisMenu/2.5.2/metisMenu.min.css" />
-
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.6/select2-bootstrap.min.css">
-
-    <link rel="stylesheet" href="{{ asset('_cms/css/app.css') }} ">
-    <link rel="stylesheet" href="{{ asset('_cms/css/featherlight.min.css') }} ">
-    <link rel="stylesheet" href="{{ asset('_cms/css/bootstrap-datetimepicker.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('_cms/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/bootstrap-theme.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/metisMenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/select2-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/featherlight.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_cms/css/bootstrap-datetimepicker.min.css') }}">
 
     @stack('javascript-head')
 
