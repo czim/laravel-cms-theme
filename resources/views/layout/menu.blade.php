58ef3b12a7782114021c5823b3c0a27b966d38c7
Coen Zimmerman <coen@pixelindustries.com>
2017-02-25 22:46:10 +0100

    Updated menu view partial for new menu repository interface

--- resources/views/layout/menu.blade.php
+++ resources/views/layout/menu.blade.php
@@ -6,16 +6,7 @@
         <div class="sidebar-nav navbar-collapse">
             <ul class="nav" id="side-menu">
 
-                @foreach ($menu->getMenuGroups() as $group)
-
-                    @include('cms::menu.group', [
-                        'group' => $group,
-                        'level' => 1,
-                    ])
-
-                @endforeach
-
-                @foreach ($menu->getMenuUngrouped() as $node)
+                @foreach ($menu->getMenuLayout() as $node)
 
                     @include('cms::menu.node', [
                         'node' => $node,
