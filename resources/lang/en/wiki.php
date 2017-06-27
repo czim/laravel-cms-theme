8a34ec41d4d0eb3128bd26e251fca531d370d86d
Coen Zimmerman <tabun@xs4all.nl>
2017-06-27 22:11:10 +0200

    Added basic wiki translations file

---
+++ resources/lang/en/wiki.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'button' => [
+        'new-page' => 'Add new page',
+    ],
+
+];
---
+++ resources/lang/nl/wiki.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'button' => [
+        'new-page' => 'Nieuwe pagina toevoegen',
+    ],
+
+];
